362842da84a0bd84bdb8b1ddae045b540b214178
emamaz <santiagomaza33@gmail.com>
2026-01-22 17:19:08 -0300

    Using the express-limit-rate library

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import routerWeather from "./routes/weather";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    message: "Too many requests, please try again after 15 minutes",
+    standardHeaders: true,
+    legacyHeaders: false,
+})
 
+app.use(limiter);
 app.use(express.json());
 app.use("/weather", routerWeather);
 
